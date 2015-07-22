54af04a7af42651ef1c61e8ca4598b58d507dae3
NaurisDabra <naurisdabra@inbox.lv>
2015-07-22 10:16:36 +0300

    Example and part of EmployeeRep implementation

---
+++ src/sef/controller/TestaKlase.java
@@ -0,0 +1,29 @@
+package sef.controller;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import sef.domain.Employee;
+import sef.interfaces.repository.EmployeeRepository;
+
+public class TestaKlase {
+
+	public TestaKlase() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:repository-config.xml");
+		EmployeeRepository customerDAO = (EmployeeRepository ) context.getBean("employeeRep");
+        
+        List<Employee> customer1 = customerDAO.findEmployeesByName("N", "");
+        for(int i=0; i<customer1.size();i++)
+        System.out.println(customer1.get(i).getFirstName());
+        System.out.println("done");
+        Employee employee=customerDAO.findEmployeeByID(1);
+        System.out.println(employee.getLastName());
+	}
+
+}
--- src/sef/impl/repository/StubEmployeeRepositoryImpl.java
+++ src/sef/impl/repository/StubEmployeeRepositoryImpl.java
@@ -1,5 +1,9 @@
 package sef.impl.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,33 +15,110 @@ import sef.interfaces.repository.EmployeeRepository;
 import org.apache.log4j.Logger;
 
 public class StubEmployeeRepositoryImpl implements EmployeeRepository {
+	// Also called DAO
 
-
-	//DataSource class encapsulates the driver, database url, username and 
-	//password information.  The dataSource object is automatically created by 
-	//the Spring framework and passed to the constructor therefore there's no need 
-	//to instantiate the dataSource variable. A connection can be acquired by 
-	//accessing the getConnection method of dataSource. 
+	// DataSource class encapsulates the driver, database url, username and
+	// password information. The dataSource object is automatically created by
+	// the Spring framework and passed to the constructor therefore there's no
+	// need
+	// to instantiate the dataSource variable. A connection can be acquired by
+	// accessing the getConnection method of dataSource.
 	//
-	//Tip: create member variables in this class that will contain the objects 
-	//passed by the Spring framework so that other methods can access the objects.
+	// Tip: create member variables in this class that will contain the objects
+	// passed by the Spring framework so that other methods can access the
+	// objects.
 
 	private static Logger log = Logger.getLogger(StubEmployeeRepositoryImpl.class);
-		
+	private DataSource dataSource;
+
 	public StubEmployeeRepositoryImpl(DataSource dataSource) {
+		this.dataSource = dataSource;
 	}
 
 	@Override
 	public List<Employee> findEmployeesByName(String firstName, String lastName) {
-		
+		String statement = "Select * from employee where firstName like ? and lastName like ?;";
+		System.out.println("statement");
 		List<Employee> list = new ArrayList<Employee>();
+		Connection conn = null;
+
+		if (lastName == null)
+			lastName = "";
+		try {
+			System.out.println("conn start");
+			conn = dataSource.getConnection();
+			System.out.println("conn got");
+			PreparedStatement ps = conn.prepareStatement(statement);
+			ps.setString(1, "%" + firstName + "%");
+			if (lastName.isEmpty())
+				ps.setString(2, "%" + lastName + "%");
+			else
+				ps.setString(2, lastName);
+			Employee employee = new Employee();
+			ResultSet rs = ps.executeQuery();
+			System.out.println("query executed");
+			while (rs.next()) {
+				System.out.println("result got");
+				employee.setID(rs.getInt("ID"));
+				employee.setFirstName(rs.getString("firstName"));
+				employee.setLastName(rs.getString("lastName"));
+				employee.setMiddleInitial(rs.getString("middleInitial"));
+				employee.setLevel(rs.getString("level"));
+				employee.setWorkForce(rs.getString("workforce"));
+				employee.setEnterpriseID(rs.getString("enterpriseID"));
+				list.add(employee);
+			}
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
 		return list;
 	}
 
 	@Override
 	public Employee findEmployeeByID(long employeeID) {
-		
 		Employee employee = new Employee();
+		String statement = "Select * from employee where ID = ?;";
+		System.out.println("statement");
+		Connection conn = null;
+		try {
+			System.out.println("conn start");
+			conn = dataSource.getConnection();
+			System.out.println("conn got");
+			PreparedStatement ps = conn.prepareStatement(statement);
+			ps.setInt(1, (int) employeeID);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				employee.setID(rs.getInt("ID"));
+				employee.setFirstName(rs.getString("firstName"));
+				employee.setLastName(rs.getString("lastName"));
+				employee.setMiddleInitial(rs.getString("middleInitial"));
+				employee.setLevel(rs.getString("level"));
+				employee.setWorkForce(rs.getString("workforce"));
+				employee.setEnterpriseID(rs.getString("enterpriseID"));
+			}
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		System.out.println("query executed");
 		return employee;
 	}
 
@@ -47,6 +128,5 @@ public class StubEmployeeRepositoryImpl implements EmployeeRepository {
 		List<Employee> list = new ArrayList<Employee>();
 		return list;
 	}
-
 
 }
