52f61ae5cf953a1da2795606e78699b91b1a989e
and3ris <andris.zundans@gmail.com>
2015-07-22 13:35:35 +0300

    Update

--- src/sef/controller/TestaKlase.java
+++ src/sef/controller/TestaKlase.java
@@ -6,7 +6,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import sef.domain.Employee;
+import sef.domain.EmployeeSkill;
 import sef.interfaces.repository.EmployeeRepository;
+import sef.interfaces.repository.SkillRepository;
 
 public class TestaKlase {
 
@@ -16,18 +18,14 @@ public class TestaKlase {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:repository-config.xml");
-		EmployeeRepository customerDAO = (EmployeeRepository) context.getBean("employeeRep");
+		SkillRepository customerDAO = (SkillRepository) context.getBean("skillRep");
 
-		List<Employee> customer1 = customerDAO.findEmployeesByName("N", "");
+		List<EmployeeSkill> customer1 = customerDAO.findEmployeeSkills(1);
 		System.out.println(customer1.toString());
 		for (int i = 0; i < customer1.size(); i++)
-			System.out.println(customer1.get(i).getFirstName());
+			System.out.println(customer1.get(i).getName());
 		System.out.println("done");
-		Employee employee = customerDAO.findEmployeeByID(1);
-		System.out.println(employee.getLastName());
-		customer1 = customerDAO.findEmployeesByProject(2);
-		for (int i = 0; i < customer1.size(); i++)
-			System.out.println(customer1.get(i).getFirstName());
+		
 	}
 
 }
--- src/sef/impl/repository/StubSkillRepositoryImpl.java
+++ src/sef/impl/repository/StubSkillRepositoryImpl.java
@@ -1,16 +1,23 @@
 package sef.impl.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.apache.log4j.Logger;
+
 import sef.domain.EmployeeSkill;
 import sef.interfaces.repository.SkillRepository;
 
-import org.apache.log4j.Logger;
-
 public class StubSkillRepositoryImpl implements SkillRepository{
+	
+	private DataSource dataSource;
+	
 
 	//DataSource class encapsulates the driver, database url, username and 
 	//password information.  The dataSource object is automatically created by 
@@ -24,12 +31,66 @@ public class StubSkillRepositoryImpl implements SkillRepository{
 	private static Logger log = Logger.getLogger(StubSkillRepositoryImpl.class);
 
 	public StubSkillRepositoryImpl(DataSource dataSource){
+		
+		this.dataSource = dataSource;
+				
 	}
+	
+	
+	
 
 	@Override
 	public List<EmployeeSkill> findEmployeeSkills(long employeeID) {
 
 		List<EmployeeSkill> list = new ArrayList<EmployeeSkill>();
+		String statement = "Select a.ID, a.name, a.description, a.rating from employeeskill a,employee b where b.ID=? and b.ID=a.employeeDetail_employee_ID; ";
+		Connection connection = null;
+		
+		try {
+			
+			connection = dataSource.getConnection();
+			PreparedStatement ps = connection.prepareStatement(statement);
+			ps.setInt(1, (int) employeeID);			
+			ResultSet rs = ps.executeQuery();
+			
+			/**
+			 * 
+			 * String statement = "Select a.ID, a.firstName, a.lastName, a.middleInitial, a.level, a.workforce,
+			 *  a.enterpriseID from employee a, employeeProjectDetail b where a.ID=b.employeeDetail_employee_ID and b.project_ID=?;";
+			 * 
+			 */
+			
+			
+			while (rs.next()) {
+				
+				EmployeeSkill skill = new EmployeeSkill();
+				skill.setID(rs.getLong("ID"));
+				skill.setName(rs.getString("name"));
+				skill.setDescription(rs.getString("description"));
+				skill.setRating(rs.getInt("rating"));
+				
+				list.add(skill);
+				
+				
+			}
+			
+			rs.close();
+			ps.close();
+			
+			
+		}catch(SQLException e) {
+			
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException ex) {
+					
+				}
+				}
+			
+		}
+		
+		
 		return list;
 	}
 
