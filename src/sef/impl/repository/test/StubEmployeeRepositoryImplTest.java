ea9249f615eedb66c9ba7697ecef9ee50646ad58
GintsZ <zarins.gints@gmail.com>
2015-07-23 11:09:58 +0300

    ...

---
+++ src/sef/impl/repository/test/StubEmployeeRepositoryImplTest.java
@@ -0,0 +1,31 @@
+package sef.impl.repository.test;
+
+import junit.framework.TestCase;
+
+public class StubEmployeeRepositoryImplTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testStubEmployeeRepositoryImpl() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindEmployeesByName() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindEmployeeByID() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindEmployeesByProject() {
+		fail("Not yet implemented");
+	}
+
+}
